7f2a075cfe02295619d4ffecd128772827358680
poker90102 <poker.king90102@gmail.com>
2020-03-04 17:00:31 +0900

    Spring Boot

--- SpringBootFundamental_R&D/SpringBootSourceCode/Part1_FundamentalSpring_SourceCode/taco-cloud/src/main/java/myindex/spring/Application.java
+++ SpringBootFundamental_R&D/SpringBootSourceCode/Part1_FundamentalSpring_SourceCode/taco-cloud/src/main/java/myindex/spring/Application.java
@@ -1,13 +1,13 @@
-package todo.project.management;
+package myindex.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManagementApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ManagementApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
